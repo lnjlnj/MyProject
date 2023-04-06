c335b06805b325d785777f9c32bff08313c3031f
leiningjie <78636567+lnjlnj@users.noreply.github.com>
2023-04-06 12:54:22 +0800

    Update classification_trainer.py

--- train_classification/vit/classification_trainer.py
+++ train_classification/vit/classification_trainer.py
@@ -1,3 +1,5 @@
+import os.path
+
 import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import AdamW, get_scheduler
@@ -5,6 +7,7 @@ import logging
 from torch.cuda.amp import GradScaler, autocast
 from tqdm import tqdm
 from PIL import Image
+from sklearn.metrics import accuracy_score
 
 
 class Trainer:
@@ -29,6 +32,7 @@ class Trainer:
 
     def train(self,
               criterion=torch.nn.CrossEntropyLoss(),
+              eval_epoch=None,
               total_epoches=10,
               batch_size=16,
               accumulation_steps=1,
@@ -37,35 +41,82 @@ class Trainer:
               weight_decay=0.1,
               eps=1e-06,
               loss_log_freq=40):
-        self.model.to(self.device)
-        self.model.train()
+        model = self.model
+        model.to(self.device)
         train_loader = DataLoader(
             dataset=self.train_dataset,
             batch_size=batch_size,
             shuffle=True)
 
         optimizer = AdamW(self.model.parameters(), lr=learning_rate)
+        eval_record = []
         for epoch in range(total_epoches):
-
+            model.train()
+            print(f'\nepoch {epoch}')
             for index, batch in enumerate(tqdm(train_loader)):
-                images = []
-                for pic in batch['pic_id']:
-                    image = Image.open(pic).convert('RGB')
-                    images.append(image)
-                try:
-                    inputs = self.processor(images=images, return_tensors="pt").to(self.device)
-                    batch['label'].to(self.device)
-                    outputs = self.model(**inputs)
-                    loss = criterion(outputs.logits, batch['label'].to(self.device))
+                image_tensor = batch['image'].to(self.device)
+                label = batch['label'].to(self.device)
+                inputs = {'pixel_values': image_tensor}
+                outputs = self.model(**inputs)
+                loss = criterion(outputs.logits, label.squeeze(1))
 
-                    optimizer.step()
+                optimizer.step()
 
-                    if accumulation_steps > 1:
-                        loss = loss / accumulation_steps
+                if accumulation_steps > 1:
+                    loss = loss / accumulation_steps
 
-                    loss.backward()
-                except:
-                    print(1)
+                loss.backward()
+            if eval_epoch is not None:
+                if epoch % eval_epoch == 0:
+                    predicts = []
+                    labels = []
+                    with torch.no_grad():
+                        model.eval()
+                        test_loader = DataLoader(
+                            dataset=self.eval_dataset,
+                            batch_size=batch_size,
+                            shuffle=True)
+                        for index, batch in enumerate(tqdm(test_loader)):
+                            image_tensor = batch['image'].to(self.device)
+                            label = batch['label'].squeeze(1).to(self.device)
+                            inputs = {'pixel_values': image_tensor}
+                            outputs = self.model(**inputs)
+                            predict = torch.argmax(outputs.logits, dim=-1)
+                            y_pred = predict.to('cpu').numpy().tolist()
+                            y_true = label.to('cpu').numpy().tolist()
+                            predicts += y_pred
+                            labels += y_true
+                        accuracy = accuracy_score(labels, predicts)
+                        eval_record.append(accuracy)
+                        if max(eval_record) == accuracy:
+                            if os.path.exists(f'./checkpoint') is False:
+                                os.mkdir(f'./checkpoint')
+                            model_save_path = f'./checkpoint/epoch{epoch}_acc_{accuracy}.pt'
+                            torch.save(model.state_dict(), model_save_path)
+                            print(f'\nepoch{epoch}达到测试集最高准确度{accuracy}，模型保存至{model_save_path}')
 
 
 
+    def eval(self, batch_size=10):
+        self.model.to(self.device)
+        predicts = []
+        labels = []
+        with torch.no_grad():
+            self.model.eval()
+            test_loader = DataLoader(
+                dataset=self.eval_dataset,
+                batch_size=batch_size,
+                shuffle=True)
+            for index, batch in enumerate(tqdm(test_loader)):
+                image_tensor = batch['image'].to(self.device)
+                label = batch['label'].squeeze(1).to(self.device)
+                inputs = {'pixel_values': image_tensor}
+                outputs = self.model(**inputs)
+                predict = torch.argmax(outputs.logits, dim=-1)
+                y_pred = predict.to('cpu').numpy().tolist()
+                y_true = label.to('cpu').numpy().tolist()
+                predicts += y_pred
+                labels += y_true
+
+            accuracy = accuracy_score(labels, predicts)
+            print(f'eval_dataset的准确率为{accuracy}')
